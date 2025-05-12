25bceaae2856d86455d9fcad9433ffed1a0e0234
Emilio <emilioelcrack987@gmail.com>
2025-05-12 00:44:30 -0500

    BORRE BAJE DE PEPA

--- VentaDirecta/Form1.cs
+++ VentaDirecta/Form1.cs
@@ -22,7 +22,7 @@ namespace VentaDirecta
             dgvVentas.Columns[3].Name = "Cantidad";
             dgvVentas.Columns[4].Name = "Total";
         }
-        //bajedepepa
+        
 
         private void btnRegistrar_Click(object sender, EventArgs e)
         {
